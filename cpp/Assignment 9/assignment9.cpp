f1539e938e82146a258246b6d0f21e699493655e
Carlos Abraham <abraham@abraham.ml>
2017-11-10 00:28:43 -0500

    Update assignment9.cpp

--- cpp/Assignment 9/assignment9.cpp
+++ cpp/Assignment 9/assignment9.cpp
@@ -1,3 +1,19 @@
+/**
+ * 
+ *      Write a program that ask the user for a positive integer no greater than 15.
+ *      The program should then display a square on the screen using the character ‘X’.
+ *      The number entered by the user will be the length of each side of the square.
+ *      For example, if the user enters 5, the program should display the following:
+ *       
+ *       XXXXX
+ *       XXXXX
+ *       XXXXX
+ *       XXXXX
+ *       XXXXX
+ *
+ *      Author: Carlos Abraham Hernandez, @19cah 
+ * 
+ */
 #include <iostream>
 using namespace std;
 
