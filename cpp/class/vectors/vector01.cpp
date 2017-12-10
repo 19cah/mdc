6d7e8e76a4cc3a1e41014d1cd298e945b5841984
Carlos Abraham <abraham@abraham.ml>
2017-12-10 02:08:19 -0500

    Update vector01.cpp

--- cpp/class/vectors/vector01.cpp
+++ cpp/class/vectors/vector01.cpp
@@ -2,3 +2,21 @@
 #include <iomanip>
 #include <vector>
 using namespace std;
+
+void enterHours(vector<int>& hours, int numOfEmployees);
+void displayHours(vector<int>& hours);
+
+int main()
+{
+    int employees;
+    vector<int> hours;
+    
+    cout << "how many employess does your company have: ";
+    cin >> employees;
+    
+    enterHours(hours, employees);
+    
+    displayHours(hours);
+    
+    return 0;
+}
