70d4fdca0ca7d50c29343c98285508d27da2dc37
Carlos Abraham <abraham@abraham.ml>
2017-12-10 02:08:34 -0500

    Update vector01.cpp

--- cpp/class/vectors/vector01.cpp
+++ cpp/class/vectors/vector01.cpp
@@ -20,3 +20,22 @@ int main()
     
     return 0;
 }
+
+void displayHours(vector<int>& hours)
+{
+    for(int i = 0; i < hours.size(); i++)
+    {
+        cout << "The hours of the employee "<< i + 1 << " is " << hours[i] << endl;
+    }
+    
+}
+void enterHours(vector<int>& hours, int numOfEmployees)
+{
+    int tempHours;
+    for(int i = 0; i < numOfEmployees; i++)
+    {
+        cout << "Please enter the hours of the employee " << i + 1 << ": "<< endl;
+        cin >> tempHours;
+        hours.push_back(tempHours);
+    }
+}
