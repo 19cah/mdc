2ed0062b4c9f7a181ed863f38b15370e293139ef
Abraham <abraham@19cah.com>
2018-01-18 17:10:40 -0500

    Update BinaryToDecimals.java

--- java/class/week2/BinaryToDecimals.java
+++ java/class/week2/BinaryToDecimals.java
@@ -1,37 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package test;
-
 /**
  *
  * @author Carlos Abraham @19cah
  */
 import java.util.Scanner;
-
-public class Test {
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
-        Scanner scan = new Scanner(System.in);
-        
-        String binaryString;
-        int binary;
-   
-        System.out.println("Enter the value in binary: ");
-        binaryString = scan.next();
-       
-        binary = Integer.parseInt(binaryString);
-            
-        for(int i = 0; i < binaryString.length(); i++){
-            
-        }
-        
+class BinaryToDecimal {
+    public static void main(String args[]){
+       Scanner input = new Scanner( System.in );
+       System.out.print("Enter a binary number: ");
+       String binaryString =input.nextLine();
+       System.out.println("Output: "+Integer.parseInt(binaryString,2));
     }
-    
 }
