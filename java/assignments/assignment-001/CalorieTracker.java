efaa79e1815cd87b3cf3da0f2068fef1c1988c7b
Carlos Abraham <carlosabraham@usa.com>
2018-01-19 01:38:33 +0000

    add content

---
+++ java/assignments/assignment-001/CalorieTracker.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Week2 {
+
+    static final int DAYS_OF_THE_WEEK = 7;
+    static final int NUMBER_OF_MEALS = 4; //
+    
+    public static void main(String[] args) {
+    
+        
+        
+        int[][] table;
+        table = new int[DAYS_OF_THE_WEEK][NUMBER_OF_MEALS];
+     
+    }
+    
+    public static int AskTheUserForMeals{
+    Scanner scan = new Scanner(System.in);
+    
+    System.out.println("Enter the ");
+    
+}
+}
