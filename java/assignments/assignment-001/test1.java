f9a0d167fec946be8e06c22e80baa1cb71fe56e8
Abraham <abraham@19cah.com>
2018-01-24 15:50:58 -0500

    Update test1.java

--- java/assignments/assignment-001/test1.java
+++ java/assignments/assignment-001/test1.java
@@ -1,10 +1,9 @@
-package calorietracker;
 
 
 import java.util.*;
 public class CalorieTracker {
 
-    static final int DAYS_OF_THE_WEEK = 7;//7;
+    static final int DAYS_OF_THE_WEEK = 3;//7;
     static final int NUMBER_OF_MEALS = 4; 
     
     public static void main(String[] args) {
@@ -31,18 +30,18 @@ public class CalorieTracker {
          
   
     printTable(table);
+    System.out.println();
     getMaxValue(table,weekDays, meals);
     getMinValue(table,weekDays, meals);
-    System.out.println("Total: "+ total(table));
-    System.out.println("Average: "+ getAverage(total(table)));
-         
+    
+    		System.out.println();
+    		for (int i = 0; i < DAYS_OF_THE_WEEK ; i++) {
+    			System.out.print( "total: "+total(table)[i] + " ");
     }
     
-    
-    
-    
-    
-    
+    System.out.println("\nAverage: "+ getAverage(total(table)));
+         
+    }
     
     /**
      *  ===========
@@ -89,78 +88,81 @@ public class CalorieTracker {
      *   Total amount of Meal
      *	========================
      */
-    public static double total(double[][] numbers) {
-        double total = 0;
+    public static double[] total(double[][] numbers) {
+        double totalPerDay = 0;
+        double[] total = new double[DAYS_OF_THE_WEEK];
         
         for(int row = 0; row < DAYS_OF_THE_WEEK; row++){
             for(int col = 0; col < NUMBER_OF_MEALS; col++){
-                total += numbers[row][col];
+                totalPerDay += numbers[row][col];
             }
+            total[row] = totalPerDay;
+            totalPerDay = 0;
         }
         return total;
     }
-    
-    
-    
     
     /**
      *  ========================
      *   Average
      *	========================
      */
-    public static double getAverage(double total) {
+    public static double getAverage(double[] total) {
         
-    		double average;
-    		average = total / (DAYS_OF_THE_WEEK * NUMBER_OF_MEALS);
-    		
-        return average;
+    		double weekTotal = 0;
+    		for(int i = 0; i < DAYS_OF_THE_WEEK; i++){
+            weekTotal += total[i];
+        }
+    		return weekTotal / DAYS_OF_THE_WEEK;
     }
     
-    
-    
-    
-    
-    /**
+   /**
      *  ========================
      *   Biggest amount of Meal
      *	========================
      */
     public static void getMaxValue(double[][] numbers, String[] days, String[] meals) {
         double maxValue = numbers[0][0];
+        String maxCalorieDay = days[0];
+        String maxCalorieMeal = meals[0];
+        
         
         for(int i = 0; i < DAYS_OF_THE_WEEK; i++){
             for(int j = 0; j < NUMBER_OF_MEALS; j++){
                 if (numbers[i][j] > maxValue) {
                     maxValue = numbers[i][j];
-                    System.out.println("biggest was "+(int)maxValue+" on "+days[i]+" at "+meals[j]);
+                    maxCalorieDay = days[i];
+                    maxCalorieMeal = meals[j];
+                    //System.out.println("biggest was "+maxValue+" on "+days[i]+" at "+meals[j]);
                 }
             }
         }
+        System.out.println("biggest was "+maxValue+" on "+ maxCalorieDay+" at "+maxCalorieMeal);
         
     }
-    
-    
-    
-    
-    
-    
     
     /**
      *  ========================
      *   Smallest amount of Meal
      *	========================
      */
-    public static void getMinValue(double[][] numbers, String[] days, String[] meals) {
-        double maxValue = numbers[0][0];
+     public static void getMinValue(double[][] numbers, String[] days, String[] meals) {
+        double minValue = numbers[0][0];
+        String minCalorieDay = days[0];
+        String minCalorieMeal = meals[0];
+        
         
         for(int i = 0; i < DAYS_OF_THE_WEEK; i++){
             for(int j = 0; j < NUMBER_OF_MEALS; j++){
-                if (numbers[i][j] < maxValue) {
-                    maxValue = numbers[i][j];
-                    System.out.println("smallest was "+ (int)maxValue+" on "+days[i]+" at "+meals[j]);
+                if (numbers[i][j] < minValue) {
+                    minValue = numbers[i][j];
+                    minCalorieDay = days[i];
+                    minCalorieMeal = meals[j];
+                    //System.out.println("biggest was "+maxValue+" on "+days[i]+" at "+meals[j]);
                 }
             }
         }
+        System.out.println("smallest was "+minValue+" on "+ minCalorieDay+" at "+minCalorieMeal);
         
     }
 }
