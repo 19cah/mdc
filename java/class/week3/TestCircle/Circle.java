c0cc45f635bcc69260ea907cbc581731c8665ded
Abraham <abraham@19cah.com>
2018-01-24 02:26:21 -0500

    Update Circle.java

--- java/class/week3/TestCircle/Circle.java
+++ java/class/week3/TestCircle/Circle.java
@@ -5,20 +5,6 @@
  */
 package testcircle;
 
-/**
- *
- * @author carlos.hernandez107
- */
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author carlos.hernandez107
- */
 public class Circle {
  
     private double radius;
