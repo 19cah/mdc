9ccdef3d6e4761197cf2c1092e490f67245fac3a
Abraham <abraham@19cah.com>
2018-01-31 13:04:32 -0500

    Update TestBankAccount.java

--- java/assignments/assignment-002/TestBankAccount.java
+++ java/assignments/assignment-002/TestBankAccount.java
@@ -1,19 +1,38 @@
 package bankaccount;
-
+import java.text.SimpleDateFormat;
+import java.util.Date;
 public class TestBankAccount {
 
+    public int car = 1;
     public static void main(String[] args){
+        
         BankAccount savingAccount = new BankAccount(20000);
         BankAccount checkingAccount = new BankAccount();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd/yyyy");
         
-        System.out.println("Account ID:" + savingAccount.getAccountId());
+        System.out.println("Account ID: " + savingAccount.getAccountId());
         System.out.println("Balance: $"+ savingAccount.getAccountBalance());
         System.out.println("Monthly Interest:  "+ savingAccount.getMonthlyInterestRate()+"%");
-        System.out.println("Created on: ");
+        System.out.println("Created on: "+simpleDateFormat.format(savingAccount.getDateCreated()));
         
+        savingAccount.withdraw(2500);
+        System.out.println("Balance after withdraw: $"+ savingAccount.getAccountBalance());
+        savingAccount.deposit(500);
+        System.out.println("Balance after deposit: $"+ savingAccount.getAccountBalance());
+        savingAccount.withdraw(10);
+        System.out.println("Balance after withdraw: $"+ savingAccount.getAccountBalance());
         
-        System.out.println("Balance after withdraw: $"+ savingAccount.withdraw(19000));
-        System.out.println("Balance after deposit: $"+ savingAccount.withdraw(2000));
+        System.out.println("Account ID: " + checkingAccount.getAccountId());
+        System.out.println("Balance: $"+ checkingAccount.getAccountBalance());
+        System.out.println("Monthly Interest:  "+ checkingAccount.getMonthlyInterestRate()+"%");
+        System.out.println("Created on: "+simpleDateFormat.format(checkingAccount.getDateCreated()));
+        
+        checkingAccount.withdraw(190);
+        System.out.println("Balance after withdraw: $"+ checkingAccount.getAccountBalance());
+        checkingAccount.deposit(500);
+        System.out.println("Balance after deposit: $"+ checkingAccount.getAccountBalance());
+        checkingAccount.withdraw(10);
+        System.out.println("Balance after withdraw: $"+ checkingAccount.getAccountBalance());
         
     }
 }
