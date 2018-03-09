2afeca12e3fac1933d55d678af172394c251321e
Abraham <abraham@19cah.com>
2018-03-09 11:13:33 -0500

    Create main.cpp

---
+++ cpp/oop/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "mother.h"
+#include "Son.h"
+
+using namespace std;
+
+int main()
+{
+	Mother lupe;
+	Son abraham;
+
+	lupe.display();
+	abraham.display();
+
+	system("pause");
+	return 0;
+}
